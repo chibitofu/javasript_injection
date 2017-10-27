8fafcd942a44be03c9665c5a59178ba7d7729fa0
chibitofu <erinmoony@gmail.com>
2017-10-26 18:10:52 -0700

    Initial Commit

---
+++ javascript_injection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  javascript_injection
+//
+//  Created by Erin Moon on 10/26/17.
+//  Copyright © 2017 Erin Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
